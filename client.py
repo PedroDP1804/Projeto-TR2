23229de45f5077282c026cec041c85cbeff35bdf
PedroDP1804 <pedropaulacampos@gmail.com>
2026-05-19 19:26:16 -0300

    feat: rate-based ABR init

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ import time
 
 config = {
     "print": True,                      # default: False
-    "multiple_bitrate_tests": True,     # default: False
+    "rate_safety_factor": 0.85,         # default: 0.8
     "address": "137.131.178.229",       # default: "137.131.178.229"
     "port": 8080                        # default: "8080"
 }
@@ -63,13 +63,45 @@ def teste_bitrate(num_qualidade:int)->int:
     return (8 * n_bytes / tempo_download) // 1000
 
 # testa na primeira qualidade disponível
-bitrate_kbps = teste_bitrate(0)
+bitrate_teste = teste_bitrate(0)
 
-if config["print"]: print(f"Bitrate: {bitrate_kbps} kbps\n")
+if config["print"]: print(f"Bitrate do teste: {bitrate_teste} kbps\n")
 
 # --------------------------------------------------------------- #
 
 
 
+# --------------------------------------------------------------- #
+#           Rate-Based ABR
+# --------------------------------------------------------------- #
+
+# margem de segurança pra escolha da bitrate
+safety_factor:float = config["rate_safety_factor"]
+
+# mapeamento das bitrates referentes às qualidades
+qualities_rates:dict[int, str] = {}
+for rep in manifesto["representations"]:
+    qualities_rates[rep["bitrate_kbps"]] = rep["quality"]
+
+if config["print"]: print(f"Bitrates das qualidades disponíveis: {qualities_rates}\n")
+
+# escolhe a maior qualidade disponível cuja bitrate seja inferior à vazão da bitrate referência, com fator de segurança
+def choose_quality(ref_rate:int)->tuple[str,int]:
+
+    chosen_quality:str = ""
+    chosen_rate:int = 0
+
+    for rate, quality in qualities_rates.items():
+        if (bitrate_teste * safety_factor >= rate):
+            chosen_quality, chosen_rate = quality, rate
+
+    return (chosen_quality, chosen_rate)
+
+# faz a primeira escolha de qualidade com base na bitrate do teste
+current_quality_rate = choose_quality(bitrate_teste)
+
+if config["print"]: print(f"Primeira Qualidade | Bitrate escolhida: {current_quality_rate[0]} | {current_quality_rate[1]} kbps\n")
+
+# --------------------------------------------------------------- #
 
 conexao.close()
